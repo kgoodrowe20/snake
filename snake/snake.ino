50d54ff9f555b043e5d7dfdb546abf2e47ca5919
kai <kgoodrowe20@punahou.edu>
2019-05-15 14:16:26 -1000

    started coding snake game
    movement added

--- snake/snake.ino
+++ snake/snake.ino
@@ -1,9 +1,73 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <MeggyJrSimple.h>
 
+int playerX, playerY;
+int appleX, appleY;
+boolean gotApple = false;
+int dir;
+
+void setup()
+{
+  MeggyJrSimpleSetup();
+  playerX = 3;
+  playerY = 4;
+  appleX = random(8);
+  appleY = random(8);
+  dir = 0;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  DrawPx(playerX, playerY, Red);
+  spawn();
+  moveplayer();
+  check();
+  DisplaySlate();
+  delay(150);
+  ClearSlate();
+}
 
+void spawn()
+{
+  DrawPx(appleX, appleY, Yellow);
+}
+
+void moveplayer()
+{
+  CheckButtonsPress();
+    if (Button_Left)
+      {
+        playerX--;
+      }
+    if (Button_Right)
+      {
+        playerX++;
+      }
+    if (Button_Down)
+      {
+        playerY--;
+      }
+    if (Button_Up)
+      {
+        playerY++;
+      }
+}
+
+void check()
+{
+  if(playerX > 7)
+  {
+    playerX = 0;
+  }
+  if(playerX < 0)
+  {
+    playerX = 7;
+  }
+  if(playerY > 7)
+  {
+    playerY = 0;
+  }
+  if(playerY < 0)
+  {
+    playerY = 7;
+  }
 }
