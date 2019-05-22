5d1fe330b3976d909840bdf66b94d2106e71971f
kai <kgoodrowe20@punahou.edu>
2019-05-21 14:24:32 -1000

    added proper snake movement
    snake now moves how it should, with the body following its head.

--- snake/snake.ino
+++ snake/snake.ino
@@ -4,6 +4,20 @@ int playerX, playerY;
 int appleX, appleY;
 boolean gotApple = false;
 int dir;
+int marker;
+
+struct Point
+{
+  int x;
+  int y;
+};
+
+Point s1 = {3,4};
+Point s2 = {4,4};
+Point s3 = {5,4};
+Point s4 = {6,4};
+
+Point snakeArray[64] = {s1, s2, s3, s4};
 
 void setup()
 {
@@ -12,13 +26,15 @@ void setup()
   playerY = 4;
   appleX = random(8);
   appleY = random(8);
-  dir = 0;
+  dir = 90;
+  marker = 4;
 }
 
 void loop()
 {
   spawn();
-  DrawPx(playerX, playerY, Red);
+  drawsnake();
+  updatesnake();
   eatapple();
   moveplayer();
   movee();
@@ -45,7 +61,56 @@ void eatapple()
   if (playerX == appleX && playerY == appleY)
     {
       gotApple = true;
+      
     }
+}
+
+void drawsnake()
+{
+  for (int i = 0; i < marker; i++)
+    {
+     DrawPx(snakeArray[i].x,snakeArray[i].y,1);
+    }
+}
+
+void updatesnake()
+{
+  for (int i = marker - 1; i > 0; i--)
+  {
+    snakeArray[i] = snakeArray[i-1];
+  }
+  if (dir == 0)
+  {
+    if (snakeArray[0].y > 6)
+    {
+      snakeArray[0].y = 0;
+    }
+    else snakeArray[0].y++;
+  }
+  if (dir == 180)
+  {
+    if (snakeArray[0].y < 1 )
+    {
+      snakeArray[0].y = 7;
+    }
+    else snakeArray[0].y--;
+  }
+  if (dir == 90)
+  {
+    if (snakeArray[0].x > 6)
+    {
+      snakeArray[0].x = 0;
+    }
+    else snakeArray[0].x++;
+  }
+    if (dir == 270)
+  {
+    if (snakeArray[0].x < 1)
+    {
+      snakeArray[0].x = 7;
+    }
+    else snakeArray[0].x--;
+  }
 }
 
 void moveplayer()
@@ -108,5 +173,3 @@ void check()
     playerY = 7;
   }
 }
-
-//struct point
