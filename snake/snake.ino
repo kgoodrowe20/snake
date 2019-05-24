5d75677fc4fabb249baec13c0a6d0160d412ee62
kai <kgoodrowe20@punahou.edu>
2019-05-23 14:18:06 -1000

    finished game
    finished snake game with working mechanics!

--- snake/snake.ino
+++ snake/snake.ino
@@ -1,9 +1,10 @@
 #include <MeggyJrSimple.h>
 
+int playerX, playerY;
 int appleX, appleY;
+boolean gotApple = false;
 int dir;
 int marker;
-boolean dead;
 
 struct Point
 {
@@ -21,86 +22,56 @@ Point snakeArray[64] = {s1, s2, s3, s4};
 void setup()
 {
   MeggyJrSimpleSetup();
+  playerX = 3;
+  playerY = 4;
   appleX = random(8);
   appleY = random(8);
-  dir = 180;
+  dir = 90;
   marker = 4;
-  dead = false;
 }
 
 void loop()
 {
-  if (dead)
-    lose();
-  else
-  {
-    eatapple();
-    drawApple();  
-    drawsnake();
-    updatesnake();
-    moveplayer();
-  }
+  spawn();
+  drawsnake();
+  updatesnake();
+  eatapple();
+  moveplayer();
+  movee();
+  check();
   DisplaySlate();
   ClearSlate();
-  delay(150);
+  delay(120);
 }
 
-void drawApple()
+void spawn()
 {
-    DrawPx(appleX, appleY, Yellow);
+  DrawPx(appleX, appleY, Yellow);
+  if (gotApple == true)
+    {
+      appleX = random(8);
+      appleY = random(8);
+      DrawPx(appleX, appleY, Yellow);
+      gotApple = false;
+    }
 }
 
 void eatapple()
 {
-  if (snakeArray[0].x == appleX && snakeArray[0].y == appleY)
+  if (playerX == appleX && playerY == appleY)
     {
-      appleX = random(8);
-      appleY = random(8);
-      marker++;
+      gotApple = true;
+      
     }
 }
-
 
 void drawsnake()
 {
   for (int i = 0; i < marker; i++)
     {
-     DrawPx(snakeArray[i].x,snakeArray[i].y,Blue);
+     DrawPx(snakeArray[i].x,snakeArray[i].y,1);
     }
-  for (int i = 1; i < marker; i++)
-    {
-     DrawPx(snakeArray[i].x,snakeArray[i].y,Red);
-    }
-  if (ReadPx(snakeArray[0].x, snakeArray[0].y) == 1)
-   {
-    dead = true;
-   }      
 }
-
-void lose()
-{
-  ClearSlate();
-  while (dead == true) 
-  {
-    for (int i = 0; i < 8; i++)
-    {
-      for (int j = 0; j < 8; j++)
-      {
-        CheckButtonsPress();
-        if (Button_A || Button_B)
-        {
-          dead = false;
-          marker = 4;
-          break;
-        }
-        DrawPx (i, j, 1);
-        DisplaySlate();
-        //delay(100);
-      }
-    }
-  }   
-} 
- 
 
 void updatesnake()
 {
@@ -161,4 +132,44 @@ void moveplayer()
       {
         dir = 0;
       }
+}
+
+void movee()
+{
+ if (dir == 270)
+    {
+      playerX--;   
+    }
+ if (dir == 90)
+    {
+      playerX++; 
+    }
+ if (dir == 0)
+    {
+      playerY++;
+    }
+ if (dir == 180)
+    {
+      playerY--; 
+    }
+}
+  
+void check()
+{
+  if(playerX > 7)
+  {
+    playerX = 0;
+  }
+  if(playerX < 0)
+  {
+    playerX = 7;
+  }
+  if(playerY > 7)
+  {
+    playerY = 0;
+  }
+  if(playerY < 0)
+  {
+    playerY = 7;
+  }
 }
