ee353c5924b839cf6312734e533201bc25195072
kai <kgoodrowe20@punahou.edu>
2019-05-23 14:17:14 -1000

    Update snake.ino

--- snake/snake.ino
+++ snake/snake.ino
@@ -1,10 +1,9 @@
 #include <MeggyJrSimple.h>
 
-int playerX, playerY;
 int appleX, appleY;
-boolean gotApple = false;
 int dir;
 int marker;
+boolean dead;
 
 struct Point
 {
@@ -22,56 +21,86 @@ Point snakeArray[64] = {s1, s2, s3, s4};
 void setup()
 {
   MeggyJrSimpleSetup();
-  playerX = 3;
-  playerY = 4;
   appleX = random(8);
   appleY = random(8);
-  dir = 90;
+  dir = 180;
   marker = 4;
+  dead = false;
 }
 
 void loop()
 {
-  spawn();
-  drawsnake();
-  updatesnake();
-  eatapple();
-  moveplayer();
-  movee();
-  check();
+  if (dead)
+    lose();
+  else
+  {
+    eatapple();
+    drawApple();  
+    drawsnake();
+    updatesnake();
+    moveplayer();
+  }
   DisplaySlate();
   ClearSlate();
-  delay(120);
+  delay(150);
 }
 
-void spawn()
+void drawApple()
 {
-  DrawPx(appleX, appleY, Yellow);
-  if (gotApple == true)
-    {
-      appleX = random(8);
-      appleY = random(8);
-      DrawPx(appleX, appleY, Yellow);
-      gotApple = false;
-    }
+    DrawPx(appleX, appleY, Yellow);
 }
 
 void eatapple()
 {
-  if (playerX == appleX && playerY == appleY)
+  if (snakeArray[0].x == appleX && snakeArray[0].y == appleY)
     {
-      gotApple = true;
-      
+      appleX = random(8);
+      appleY = random(8);
+      marker++;
     }
 }
+
 
 void drawsnake()
 {
   for (int i = 0; i < marker; i++)
     {
-     DrawPx(snakeArray[i].x,snakeArray[i].y,1);
+     DrawPx(snakeArray[i].x,snakeArray[i].y,Blue);
     }
+  for (int i = 1; i < marker; i++)
+    {
+     DrawPx(snakeArray[i].x,snakeArray[i].y,Red);
+    }
+  if (ReadPx(snakeArray[0].x, snakeArray[0].y) == 1)
+   {
+    dead = true;
+   }      
 }
+
+void lose()
+{
+  ClearSlate();
+  while (dead == true) 
+  {
+    for (int i = 0; i < 8; i++)
+    {
+      for (int j = 0; j < 8; j++)
+      {
+        CheckButtonsPress();
+        if (Button_A || Button_B)
+        {
+          dead = false;
+          marker = 4;
+          break;
+        }
+        DrawPx (i, j, 1);
+        DisplaySlate();
+        //delay(100);
+      }
+    }
+  }   
+} 
+ 
 
 void updatesnake()
 {
@@ -132,44 +161,4 @@ void moveplayer()
       {
         dir = 0;
       }
-}
-
-void movee()
-{
- if (dir == 270)
-    {
-      playerX--;   
-    }
- if (dir == 90)
-    {
-      playerX++; 
-    }
- if (dir == 0)
-    {
-      playerY++;
-    }
- if (dir == 180)
-    {
-      playerY--; 
-    }
-}
-  
-void check()
-{
-  if(playerX > 7)
-  {
-    playerX = 0;
-  }
-  if(playerX < 0)
-  {
-    playerX = 7;
-  }
-  if(playerY > 7)
-  {
-    playerY = 0;
-  }
-  if(playerY < 0)
-  {
-    playerY = 7;
-  }
 }
